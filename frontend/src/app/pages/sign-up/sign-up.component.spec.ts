df001d474263bc7745090a1817f165a76e4926fa
Zohar Sabari <zohar1000@gmail.com>
2021-05-19 06:20:37 +0300

    made unit test for sign up component

--- frontend/src/app/pages/sign-up/sign-up.component.spec.ts
+++ frontend/src/app/pages/sign-up/sign-up.component.spec.ts
@@ -1,14 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SignUpComponent } from './sign-up.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from '../../app.component';
 
-describe('SignUpComponent', () => {
-  let component: SignUpComponent;
+fdescribe('SignUpComponent', () => {
   let fixture: ComponentFixture<SignUpComponent>;
+  let component: SignUpComponent;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SignUpComponent ]
+      declarations: [ SignUpComponent ],
+      imports: [ReactiveFormsModule, HttpClientTestingModule, RouterTestingModule]
     })
     .compileComponents();
   });
@@ -23,5 +28,19 @@ describe('SignUpComponent', () => {
     expect(component).toBeTruthy();
   });
 
+  it(`should return empty string`, () => {
+    expect(component.checkFormValidation('Didi', 'Manusi')).toEqual('');
+  });
 
+  it(`should fail for too short first name`, () => {
+    expect(component.checkFormValidation('D', 'Manusi')).toEqual('first name should be at least 2 characters');
+  });
+
+  it(`should fail for too long family name`, () => {
+    expect(component.checkFormValidation('Didi', 'Manusikjkklljkljkljkjlkjkjkljlkj')).toEqual('last name should be at most 15 characters');
+  });
+
+  it(`should fail for empty parameters`, () => {
+    expect(component.checkFormValidation('', '')).not.toEqual('');
+  });
 });
